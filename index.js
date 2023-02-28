c1fd4d99fd2860d32dbb6287a458a37dc4b6dc26
EndlessEndless1999 <115580045+EndlessEndless1999@users.noreply.github.com>
2023-02-28 05:22:27 +0000

    populates questions

--- index.js
+++ index.js
@@ -5,7 +5,14 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
-
+    'What is the title of your Project?',
+    'Enter a Table of Contents Seperated by Spaces.',
+    'Please Describe the installation process.',
+    'Please Describe the usage of the app.',
+    'Please Describe how others can contribute.',
+    'Describe any Tests',
+    'Please describe where users can look for FAQs',
+    'Please choose a license.'
 ];
 
 // function to write README file
