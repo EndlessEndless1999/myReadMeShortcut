e1baab46c83d24b6fa21cbf87f19814829ab1978
EndlessEndless1999 <115580045+EndlessEndless1999@users.noreply.github.com>
2023-03-09 01:54:00 +0000

    intialises inquirer

--- index.js
+++ index.js
@@ -1,19 +1,9 @@
 const fs = require("fs");
 const path = require('path');
-const inquirer = require("inquirer");
+const inquirer = require('inquirer');
 const generateMarkdown = require("./utils/generateMarkdown");
 
-// array of questions for user
-const questions = [
-    'What is the title of your Project?',
-    'Enter a Table of Contents Seperated by Spaces.',
-    'Please Describe the installation process.',
-    'Please Describe the usage of the app.',
-    'Please Describe how others can contribute.',
-    'Describe any Tests',
-    'Please describe where users can look for FAQs',
-    'Please choose a license.'
-];
+
 
 // function to write README file
 function writeToFile(fileName, data) {
@@ -21,8 +11,39 @@ function writeToFile(fileName, data) {
 
 // function to initialize program
 function init() {
-
+    inquirer
+    .prompt([
+        {   type: 'input',
+            message: 'What is the title of your Project?',
+            name: '1'},
+        {   type: 'input',
+            message: 'Enter a Table of Contents Seperated by Spaces.',
+            name: '2'},
+        {   type: 'input',
+            message: 'Please Describe the installation process.',
+            name: '3'},
+        {   type: 'input',
+            message: 'Please Describe the usage of the app.',
+            name: '4'},
+        {   type: 'input',
+            message: 'Please Describe how others can contribute.',
+            name: '5'},
+        {   type: 'input',
+            message: 'Describe any Tests',
+            name: '6'},
+        {   type: 'input',
+            message: 'Please describe where users can look for FAQs',
+            name: '7'},
+        {   type: 'input',
+            message: 'Please choose a license.',
+            name: '8'}
+    ])
+    .then((answers) => {
+      // Use user feedback for... whatever!!
+      console.log(answers);
+    });
 }
+
 
 // function call to initialize program
 init();
