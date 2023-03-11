4f32e6975271e0287e6f30af721d535ca81bc1ba
EndlessEndless1999 <115580045+EndlessEndless1999@users.noreply.github.com>
2023-03-11 01:32:08 +0000

    adds working function

---
+++ TESTREADME.md
@@ -0,0 +1,38 @@
+
+# h
+![badge](https://img.shields.io/badge/license-MIT-brightgreen)
+
+
+
+## Description
+h
+
+
+
+
+## Table of Contents
+- [Description]
+- [Installation]
+- [Usage]
+- [License]
+- [Contributing]
+- [Tests]
+- [Questions]
+
+
+
+
+## Installation
+h
+## Usage
+h
+## License
+![badge](https://img.shields.io/badge/license-MIT-brightgreen)
+This application is covered by the MIT license. 
+## Contributing
+h
+## Tests
+h
+## Questions
+h
+GitHub: [h](https://github.com/h)
--- index.js
+++ index.js
@@ -5,9 +5,6 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 
 
-// function to write README file
-function writeToFile(fileName, data) {
-}
 
 // function to initialize program
 function init() {
@@ -15,35 +12,91 @@ function init() {
     .prompt([
         {   type: 'input',
             message: 'What is the title of your Project?',
-            name: '1'},
-        {   type: 'input',
-            message: 'Enter a Table of Contents Seperated by Spaces.',
-            name: '2'},
+            name: 'title'},
+        {
+            type: 'input',
+            message: 'Please provide a description',
+            name: 'description'
+        },
         {   type: 'input',
             message: 'Please Describe the installation process.',
-            name: '3'},
+            name: 'installation'
+        },
         {   type: 'input',
             message: 'Please Describe the usage of the app.',
-            name: '4'},
+            name: 'usage'
+        },
         {   type: 'input',
             message: 'Please Describe how others can contribute.',
-            name: '5'},
+            name: 'contribute'
+        },
         {   type: 'input',
             message: 'Describe any Tests',
-            name: '6'},
+            name: 'tests'
+        },
         {   type: 'input',
             message: 'Please describe where users can look for FAQs',
-            name: '7'},
-        {   type: 'input',
+            name: 'question'
+        },
+        {   type: 'list',
             message: 'Please choose a license.',
-            name: '8'}
+            name: 'license',
+            choices: ['MIT', 'MPL', 'Perl', 'IPL', 'Apache', 'Boost']
+        },
+        {
+            type: 'input',
+            message: 'Please provide your Github username',
+            name: 'username'
+        }
     ])
     .then((answers) => {
       // Use user feedback for... whatever!!
       console.log(answers);
+      const generateContent = generateReadme(answers);
+      fs.writeFileSync('./TESTREADME.md', generateContent);
     });
 }
 
+function generateReadme(answers) {
+    return `
+# ${answers.title}
+![badge](https://img.shields.io/badge/license-${answers.license}-brightgreen)
+
+
+
+## Description
+${answers.description}
+
+
+
+
+## Table of Contents
+- [Description]
+- [Installation]
+- [Usage]
+- [License]
+- [Contributing]
+- [Tests]
+- [Questions]
+
+
+
+
+## Installation
+${answers.installation}
+## Usage
+${answers.usage}
+## License
+![badge](https://img.shields.io/badge/license-${answers.license}-brightgreen)
+This application is covered by the ${answers.license} license. 
+## Contributing
+${answers.contribute}
+## Tests
+${answers.tests}
+## Questions
+${answers.question}
+GitHub: [${answers.username}](https://github.com/${answers.username})`
+  }
 
 // function call to initialize program
 init();
